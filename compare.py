ffa00fae10cb516b0a614fdd81c984a9764c095f
Damian Shaw <damian.peter.shaw@gmail.com>
2024-11-17 18:50:29 -0500

    switch to resolution statistics (from collection statistics)

--- compare.py
+++ compare.py
@@ -8,12 +8,21 @@
 import json
 import os
 import tomllib
-from collections import defaultdict
 from pathlib import Path
 
 import typer
 
 SCENARIOS_DIR = "scenarios"
+
+
+def percent_change(value_1: int, value_2: int) -> str:
+    if value_1 == 0:
+        if value_2 == 0:
+            return "100%"
+        else:
+            return "inf%"
+
+    return f"{(value_2 * 100)/value_1:.2f}%"
 
 
 def process_toml_file(toml_file: Path, pip_name_1: str, pip_name_2: str) -> None:
@@ -76,52 +85,113 @@ def process_toml_file(toml_file: Path, pip_name_1: str, pip_name_2: str) -> None
         failure_reason_1 = json_1["result"]["failure_reason"]
         failure_reason_2 = json_2["result"]["failure_reason"]
         install_info_1 = {
-            install["file_name"]: install["hash"]
+            install["file"]: install["hash"]
             for install in json_1["result"]["install_info"]
         }
         install_info_2 = {
-            install["file_name"]: install["hash"]
+            install["file"]: install["hash"]
             for install in json_2["result"]["install_info"]
         }
 
-        resolution_1 = defaultdict(list)
-        for resolution_step in json_1["resolution"]:
-            resolution_1[resolution_step["requirement"]].extend(
-                resolution_step["packages"]
-            )
+        wheels_1: set[str] = set()
+        sdists_1: set[str] = set()
+        visited_packages_1 = 0
+        visited_requirements_1 = 0
+        rejected_rquirements_1 = 0
+        number_pinned_1 = 0
+        number_rounds_1 = len(json_1["resolution_rounds"])
+        for resolution_round_1 in json_1["resolution_rounds"]:
+            if "pinned" in resolution_round_1:
+                number_pinned_1 += 1
+            if "added" in resolution_round_1:
+                visited_packages_1 += len(resolution_round_1["added"])
+                wheels_1.update(
+                    w for w in resolution_round_1["added"] if w.endswith(".whl")
+                )
+                sdists_1.update(
+                    s for s in resolution_round_1["added"] if not s.endswith(".whl")
+                )
+                for added_requirements_1 in resolution_round_1["added"].values():
+                    visited_requirements_1 += len(added_requirements_1)
+            if "rejected" in resolution_round_1:
+                rejected_rquirements_1 += len(resolution_round_1["rejected"])
 
-        resolution_2 = defaultdict(list)
-        for resolution_step in json_2["resolution"]:
-            resolution_2[resolution_step["requirement"]].extend(
-                resolution_step["packages"]
-            )
+        wheels_2: set[str] = set()
+        sdists_2: set[str] = set()
+        visited_packages_2 = 0
+        visited_requirements_2 = 0
+        rejected_rquirements_2 = 0
+        number_pinned_2 = 0
+        number_rounds_2 = len(json_2["resolution_rounds"])
+        for resolution_round_2 in json_2["resolution_rounds"]:
+            if "pinned" in resolution_round_2:
+                number_pinned_2 += 1
+            if "added" in resolution_round_2:
+                visited_packages_2 += len(resolution_round_2["added"])
+                wheels_2.update(
+                    w for w in resolution_round_2["added"] if w.endswith(".whl")
+                )
+                sdists_2.update(
+                    s for s in resolution_round_2["added"] if not s.endswith(".whl")
+                )
+                for added_requirements_2 in resolution_round_2["added"].values():
+                    visited_requirements_2 += len(added_requirements_2)
+            if "rejected" in resolution_round_2:
+                rejected_rquirements_2 += len(resolution_round_2["rejected"])
 
         difference_messages = []
         one_failed = success_1 != success_2
         if one_failed:
-            difference_messages.append(f"Success: {success_1} -> {success_2}.")
+            difference_messages.append(f"Success: {success_1} -> {success_2}")
 
         if failure_reason_1 != failure_reason_2:
             difference_messages.append(
-                f"Failure Reason: {failure_reason_1} -> {failure_reason_2}."
+                f"Failure Reason: {failure_reason_1} -> {failure_reason_2}"
             )
-    
-        if not one_failed and install_info_1 != install_info_2:
-            difference_messages.append("Not the same install files.")
+        elif failure_reason_1 and failure_reason_1 == failure_reason_2:
+            difference_messages.append(f"Both failed: {failure_reason_1}")
 
-        if resolution_1 != resolution_2:
-            num_requirements_1 = len(resolution_1.keys())
-            num_requirements_2 = len(resolution_2.keys())
-            if num_requirements_1 != num_requirements_2:
+        if not one_failed and install_info_1 != install_info_2:
+            difference_messages.append("Not the same install files")
+
+        if json_1["resolution_rounds"] != json_2["resolution_rounds"]:
+            if len(sdists_1) != len(sdists_2):
                 difference_messages.append(
-                    f"Number of requirements processed: {num_requirements_1} -> {num_requirements_2}"
+                    f"Distinct Sdists visisted: {len(sdists_1)} -> {len(sdists_2)} ({percent_change(len(sdists_1), len(sdists_2))})"
+                )
+            elif len(sdists_1) != len(sdists_2):
+                difference_messages.append("Different distinct sdists visisted")
+
+            if len(wheels_1) != len(wheels_2):
+                difference_messages.append(
+                    f"Distinct Wheels visisted: {len(wheels_1)} -> {len(wheels_2)} ({percent_change(len(wheels_1), len(wheels_2))})"
+                )
+            elif len(wheels_1) != len(wheels_2):
+                difference_messages.append("Different distinct wheels visisted")
+
+            if visited_packages_1 != visited_packages_2:
+                difference_messages.append(
+                    f"Total visisted packages: {visited_packages_1} -> {visited_packages_2} ({percent_change(visited_packages_1, visited_packages_2)})"
                 )
 
-            num_packages_1 = sum(len(x) for x in resolution_1.values())
-            num_packages_2 = sum(len(x) for x in resolution_2.values())
-            if num_packages_1 != num_packages_2:
+            if visited_requirements_1 != visited_requirements_2:
                 difference_messages.append(
-                    f"Number of packages processed: {num_packages_1} -> {num_packages_2}"
+                    f"Total visisted requirements: {visited_requirements_1} -> {visited_requirements_2} ({percent_change(visited_requirements_1, visited_requirements_2)})"
+                )
+
+            if rejected_rquirements_1 != rejected_rquirements_2:
+                difference_messages.append(
+                    f"Total rejected requirements: {rejected_rquirements_1} -> {rejected_rquirements_2} ({percent_change(rejected_rquirements_1, rejected_rquirements_2)})"
+                )
+
+            if number_pinned_1 != number_pinned_2:
+                difference_messages.append(
+                    f"Total pinned packages: {number_pinned_1} -> {number_pinned_2} ({percent_change(number_pinned_1, number_pinned_2)})"
+                )
+
+            if number_rounds_1 != number_rounds_2:
+                difference_messages.append(
+                    f"Total rounds: {number_rounds_1} -> {number_rounds_2} ({percent_change(number_rounds_1, number_rounds_2)})"
                 )
 
         if difference_messages:
@@ -161,7 +231,7 @@ def main(
 
     scenarios_path = Path(SCENARIOS_DIR)
     if not scenarios_path.exists() or not scenarios_path.is_dir():
-        print(f"The directory '{SCENARIOS_DIR}' does not exist.")
+        print(f"The directory '{SCENARIOS_DIR}' does not exist")
         return
 
     # Loop through each TOML file in the scenarios directory
